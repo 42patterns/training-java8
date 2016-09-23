307bf59a0e5c2c249e7b1cf7adc7b363efdc73b4
kubamarchwicki <kuba@marchwicki.pl>
2016-09-23 11:02:18 +0200

    Ex2x: Misc Java8 improvements

---
+++ src/main/java/com/example/foo/java8/LoremIpsum.java
@@ -0,0 +1,41 @@
+package com.example.foo.java8;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+/**
+ * @see Map#merge(Object, Object, BiFunction)
+ * @see Map#computeIfAbsent(Object, Function)
+ * @see Collectors#groupingBy(Function)
+ */
+public class LoremIpsum {
+
+	public static String text() throws IOException {
+		return IOUtils.toString(LoremIpsum.class.getResourceAsStream("/lorem-ipsum.txt"));
+	}
+
+	/**
+	 * Case insensitive
+	 */
+	public static Map<String, Integer> wordCount(String text) {
+		throw new UnsupportedOperationException("wordCount()");
+	}
+
+	private static List<String> splitWords(String text) {
+        final String[] split = text.split("[ \\.,\n]");
+        return Arrays.stream(split)
+				.map(s -> s.trim())
+				.filter(s -> !s.isEmpty())
+				.collect(Collectors.toList());
+	}
+
+}
